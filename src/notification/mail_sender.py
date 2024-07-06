b2a4de00b9163ffc50f6086874d7ca17943846a8
1Wggb1 <1Wggb1>
2024-07-06 12:12:26 -0300

    [SCRAPING] Change hard-coded instanciation of scraping providers using environment variable

--- src/notification/mail_sender.py
+++ src/notification/mail_sender.py
@@ -1,28 +1,16 @@
-import json
 import smtplib
 from email.message import EmailMessage
 import os
 from typing import Final
-from src.util.map_util import get_key_or_default
-from src.util.logger_util import log
 
 
 class MailSender:
     FROM: Final = os.environ["EMAIL"]
     FROM_ID: Final = os.environ["EMAIL_ID"]
-    PREFERENCES: Final = os.environ["PREFERENCES"]
 
-    def send(self, provider_name, content, car_model):
-        preferences = json.loads(MailSender.PREFERENCES)
-        model_preferences = get_key_or_default(preferences, car_model.lower())
-        if not model_preferences:
-            log.warn("Model preferences not found")
-            return
-        recipients = get_key_or_default(model_preferences, "recipients")
-        if not recipients:
-            log.warn("Model recipients not found")
-            return
+    def send(self, provider_name, content, car_model, recipients):
 
+        print(provider_name, content, car_model, recipients)
         msg = EmailMessage()
         msg["Subject"] = f"New ad´s found of {car_model} on {provider_name}"
         msg["From"] = MailSender.FROM
