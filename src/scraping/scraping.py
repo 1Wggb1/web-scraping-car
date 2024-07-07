11298903dc836f5c5e617cb407391e666bdcde6f
Willian Garbo <50598569+1Wggb1@users.noreply.github.com>
2024-07-07 16:41:42 -0300

    Update scraping.py

--- src/scraping/scraping.py
+++ src/scraping/scraping.py
@@ -9,8 +9,8 @@ class Scraping:
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5",
         "Accept-Encoding": "gzip, deflate",
-        "Connection": "keep-alive",
-        "Cookie": "_dd_s=rum=0&expire=1720380005083"}
+        "Cookie": "_dd_s=rum=0&expire=1720380005083",
+        "Upgrade-Insecure-Requests": "1"}
 
     def search(self, site_url):
         scraping_content = requests.get(site_url, headers=Scraping.FAKE_AGENT_HEADER).content
