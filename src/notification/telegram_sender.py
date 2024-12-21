8a55e4d4a8f00cd9666c7b2b65916b197dac2487
1Wggb1 <willianggbatista@gmail.com>
2024-12-21 11:15:43 -0300

    [SCRAPING] Improving telegram message format

--- src/notification/telegram_sender.py
+++ src/notification/telegram_sender.py
@@ -10,6 +10,16 @@ class TelegramSender:
     def send(self, provider_name, content, car_model):
         try:
             bot = Bot(TelegramSender.BOT_TOKEN)
-            bot.sendMessage(TelegramSender.CHAT_ID, f"<b>{provider_name.upper()} - CAR ADS FOUND</b>\n\n- MODEL = {car_model}\n\n<code>{content}</code>", parse_mode="HTML")
+            bot.sendMessage(TelegramSender.CHAT_ID, self.__create_telegram_message(provider_name, content, car_model), parse_mode="HTML")
         except Exception as exception:
             log.error(f"Error when sending telegram message. Error message = {exception.__str__()}")
+
+    def __create_telegram_message(self, provider_name, content, car_model):
+        message_header = f"<b>{provider_name.upper()} - CAR ADS FOUND\n\U0001F698 {car_model.upper()}</b>\n\n"
+        message_body = ""
+        for url, result in content.items():
+            message_body += f"\U0001F697\nlink: {url}\n"
+            for car_spec, value in result.items():
+                message_body += f"{car_spec}: {value}\n"
+            message_body += "\n"
+        return message_header + message_body
